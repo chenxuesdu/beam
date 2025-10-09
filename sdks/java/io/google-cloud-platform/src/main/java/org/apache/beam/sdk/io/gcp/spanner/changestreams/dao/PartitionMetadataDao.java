0974f21bb01a7a39bb0d0af423f6ce0d9c15b9c8
Xue Chen <xuechen@google.com>
2025-10-09 18:16:22 +0000

    Only update watermark if the new value is larger than the existing value
    The updateWatermark is called in
    QueryChangeStreamAction.updateWatermarkCallback.This funtion update the
    watermark in a async way, so the update order is not guaranteed.
    
    Updated the update watermark logic so that we only move the watermark of
    one partition forward.

--- sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/changestreams/dao/PartitionMetadataDao.java
+++ sdks/java/io/google-cloud-platform/src/main/java/org/apache/beam/sdk/io/gcp/spanner/changestreams/dao/PartitionMetadataDao.java
@@ -528,14 +528,22 @@ public class PartitionMetadataDao {
     }
 
     /**
-     * Update the partition watermark to the given timestamp.
+     * Update the partition watermark to the given timestamp if the given timestamp is larger than
+     * the existing value.
      *
      * @param partitionToken the partition unique identifier
      * @param watermark the new partition watermark
      * @return the commit timestamp of the read / write transaction
      */
     public Void updateWatermark(String partitionToken, Timestamp watermark) {
-      transaction.buffer(createUpdateMetadataWatermarkMutationFrom(partitionToken, watermark));
+      Timestamp currentWatermark = getWatermarkForPartition(partitionToken);
+      if (currentWatermark == null) {
+        LOG.debug("Partiton {} cannot find.", partitionToken);
+        return null;
+      }
+      if (watermark.compareTo(currentWatermark) > 0) {
+        transaction.buffer(createUpdateMetadataWatermarkMutationFrom(partitionToken, watermark));
+      }
       return null;
     }
 
@@ -581,7 +589,51 @@ public class PartitionMetadataDao {
         return null;
       }
     }
-
+    
+     /**
+     * Fetches the watermark for a given partition token.
+     *
+     * @param partitionToken the partition unique identifier
+     * @return the watermark for the given token if it exists. Otherwise, it returns null.
+     */
+    public @Nullable Timestamp getWatermarkForPartition(String partitionToken) {
+      final Statement statement;
+      if (this.dialect == Dialect.POSTGRESQL) {
+        statement =
+            Statement.newBuilder(
+                    "SELECT \""
+                        + COLUMN_WATERMARK
+                        + "\" FROM \""
+                        + metadataTableName
+                        + "\" WHERE \""
+                        + COLUMN_PARTITION_TOKEN
+                        + "\" = $1")
+                .bind("p1")
+                .to(partitionToken)
+                .build();
+      } else {
+        statement =
+            Statement.newBuilder(
+                    "SELECT "
+                        + COLUMN_WATERMARK
+                        + " FROM "
+                        + metadataTableName
+                        + " WHERE "
+                        + COLUMN_PARTITION_TOKEN
+                        + " = @partition")
+                .bind("partition")
+                .to(partitionToken)
+                .build();
+      }
+      try (ResultSet resultSet =
+          transaction.executeQuery(statement, Options.tag("query=getWatermarkForPartition"))) {
+        if (resultSet.next()) {
+          return resultSet.getTimestamp(COLUMN_WATERMARK);
+        }
+        return null;
+      }
+    }
+    
     private Mutation createInsertMetadataMutationFrom(PartitionMetadata partitionMetadata) {
       return Mutation.newInsertBuilder(metadataTableName)
           .set(COLUMN_PARTITION_TOKEN)
